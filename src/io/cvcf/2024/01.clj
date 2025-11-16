52818826f2cce55e08642cd0d002e776d660ec3c
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2025-11-16 13:08:31 -0600

    Solve Day 01 part 2

--- src/io/cvcf/2024/01.clj
+++ src/io/cvcf/2024/01.clj
@@ -1,18 +1,25 @@
 (ns io.cvcf.2024.01
   (:require
+   [clojure.string :as s]
    [io.cvcf.2024.solve :refer [solve]]
-   [io.cvcf.files :as files]
-   [clojure.string :as str]))
+   [io.cvcf.files :as files]))
 
 (defn parse-input
   [input]
   (let [data (map (comp #(map parse-long %)
-                        #(str/split % #" +"))
-                  (str/split-lines input))]
+                        #(s/split % #" +"))
+                  (s/split-lines input))]
     [(mapv first data)
      (mapv second data)]))
 
 (defmethod solve 1
   [_ input]
   (let [[d1 d2] (parse-input input)]
-    (reduce + (map #(abs (- %1 %2)) (sort d1) (sort d2)))))
+    (str "p1: " (reduce + (map #(abs (- %1 %2)) (sort d1) (sort d2)))
+         "\n"
+         "p2: " (reduce + (map (fn [d] (* d (count (filter #(= d %) d2)))) d1)))))
+
+(comment
+  (println (solve 1 (files/slurp-resource "inputs/2024/01-test")))
+
+  ::end)
